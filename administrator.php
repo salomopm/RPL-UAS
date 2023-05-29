90476f3d9a2c5575828e56f3edf5c168e2322ad0
salomopm <salomopm26@gmail.com>
2023-05-30 01:29:38 +0700

    admin minus log out

--- administrator.php
+++ administrator.php
@@ -12,77 +12,43 @@ require 'db_functions.php';
     <link rel="stylesheet" href="/assets/css/adminstyle.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
-<body>
-    <nav class="navbar navbar-dark bg-dark fixed-top">
+<body style="background:url('assets/img/bg-admin.jpg') no-repeat; background-size: 2000px 1000px;overflow-x: hidden;">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <nav class="navbar bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="administrator.php">
+                <img src="assets/img/logo_uns.png" alt="Logo" width="300" height="100" class="d-inline-block align-text-top">
+                </a>
+            </div>
+        </nav>
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">UNS Administrator</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
-            <div class="offcanvas-header">
-                <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Admin Panel</h5>
-                <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-            </div>
-            <div class="offcanvas-body">
-                <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="administrator.php">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="http://localhost:3000/man_mah.php">Manajemen Akun User</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="unggahinformasi.php">Unggah Informasi Prodi</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="man_info.php">Informasi Prodi</a>
-                </li>
-                </ul>
-            </div>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <div class="nav-link1">
+                        <a class="nav-link active" aria-current="page" href="administrator.php">Beranda</a>
+                    </div>
+                    <div class="nav-link2">
+                        <a class="nav-link active" aria-current="page" href="unggahinformasi.php">Unggah</a>
+                    </div>
+                    <div class="nav-link3">
+                        <a class="nav-link active" aria-current="page" href="man_info.php">Informasi</a>
+                    </div>
+                    <div class="nav-link4">
+                        <a class="nav-link active" aria-current="page" href="man_mah.php">Menghapus</a>
+                    </div>
+                </div>
             </div>
         </div>
     </nav>
     <div class="main-panel">
         <div class="dashboard">
-            <h2 class="dashboard-text">Dashboard</h2>
-            <div class="row1">
-                <div class="card1">
-                    <div class="card" style="width: 25rem; height: 10rem;">
-                        <div class="svg-container-people">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-people" viewBox="0 0 16 16">
-                                <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1h8Zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022ZM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4Zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816ZM4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275ZM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0Zm3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4Z"/>
-                            </svg>
-                        </div>
-                        <div class="card-body">
-                            <?php 
-
-                                $sql = "SELECT * from db_mahasiswaunsharing";
-                                if ($result = mysqli_query($conn, $sql)) {
-                                $rowcount = mysqli_num_rows( $result );
-                                echo '<p class="total-user-angka">';
-                                echo $rowcount;
-                                echo '</p>';
-                                }
-                            
-                            ?>
-                            <p class="total-user-text">Total User</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="card2">
-                    <div class="card" style="width: 25rem; height: 10rem;">
-                        <div class="svg-container-info">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-info" viewBox="0 0 16 16">
-                                <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
-                            </svg>
-                        </div>
-                        <div class="card-body">
-                            <!-- taro total info yg di -->
-                            <p class="total-user-text-info">Total Informasi</p>
-                        </div>
-                    </div>
-                </div>
+            <div class="dashboard-text">
+                Selamat Datang di <br>
+                Sistem Berbagi <br>
+                Informasi UNS
+            </div>
+            <div class="dashboard-image">
+                <img src="assets/img/dashboard.png" class="dashboard-img" alt="">
             </div>
         </div>
     </div>
